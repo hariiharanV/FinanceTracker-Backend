a696290a65fd240de4b5bc5f090b0e037623ec73
hari <harii1996@gmail.com>
2025-02-16 19:11:35 +0530

    first commit

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -32,8 +32,8 @@ export const register = async(req,res)=>{
 
         res.cookie('token',token,{
             httpOnly:true,
-            secure:process.env.ENV_NODE === 'production',
-            sameSite: process.env.ENV_NODE === 'production'?'none':'strict',
+            secure:process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production'?'none':'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
@@ -85,8 +85,8 @@ export const login = async(req,res)=>{
 
         res.cookie('token',token,{
             httpOnly:true,
-            secure:process.env.ENV_NODE === 'production',
-            sameSite: process.env.ENV_NODE === 'production'?'none':'strict',
+            secure:process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production'?'none':'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
@@ -107,8 +107,8 @@ export const logout = async(req,res)=>{
 
         res.clearCookie('token',{
             httpOnly:true,
-            secure:process.env.ENV_NODE === 'production',
-            sameSite: process.env.ENV_NODE === 'production'?'none':'strict'})
+            secure:process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production'?'none':'strict'})
 
             return res.json({success:true,message:"Logged out"})
 
